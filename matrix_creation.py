55adbc7a53c3ab00c890fd786b5fed34920f81a3
shrebox <shreyash15097@iiitd.ac.in>
2018-10-20 21:15:09 +0530

    with semester dictionary updated

--- matrix_creation.py
+++ matrix_creation.py
@@ -11,7 +11,7 @@ with open('data/csv/1.csv') as csv_file:
         if line_count != 0:
             course_code = row[len(row)-6]
             if course_code not in subjects_list:
-                subjects_list[course_code] = 0
+                subjects_list[(course_code,'semester')] = (0,0)
             grade_val = row[len(row)-3]
             if grade_val not in grades:
                 grades[grade_val] = 0
@@ -47,133 +47,139 @@ with open('data/csv/1.csv') as csv_file:
                 mat1[student_val] = temp
             course_code = row[len(row)-6]
             grade_val = grade_scale[row[len(row)-3]]
-            mat1[student_val][course_code] = grade_val
             semester_val = row[2].split()
-            semester_val = semester_val[len(semester_val)-1]
-            mat1[student_val]['semester'] = semester_val
+            semester_val = int(semester_val[len(semester_val)-1])
+            mat1[student_val][(course_code,'semester')] = (grade_val,semester_val)
+            # mat1[student_val]['semester'] = semester_val
         line_count+=1
 
-addval = len(mat1)
-with open('data/csv/2.csv') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    line_count = 0
-    for row in csv_reader:
-        if line_count != 0:
-            student_val = row[1]
-            student_val = int(student_val.split('Student ')[1]) + addval
-            if student_val not in mat1:
-                temp = copy.deepcopy(subjects_list)
-                mat1[student_val] = temp
-            course_code = row[len(row)-6]
-            grade_val = grade_scale[row[len(row)-3]]
-            mat1[student_val][course_code] = grade_val
-            semester_val = row[2].split()
-            semester_val = semester_val[len(semester_val)-1]
-            mat1[student_val]['semester'] = semester_val
-        line_count+=1
+# addval = len(mat1)
+# with open('data/csv/2.csv') as csv_file:
+#     csv_reader = csv.reader(csv_file, delimiter=',')
+#     line_count = 0
+#     for row in csv_reader:
+#         if line_count != 0:
+#             student_val = row[1]
+#             student_val = int(student_val.split('Student ')[1]) + addval
+#             if student_val not in mat1:
+#                 temp = copy.deepcopy(subjects_list)
+#                 mat1[student_val] = temp
+#             course_code = row[len(row)-6]
+#             grade_val = grade_scale[row[len(row)-3]]
+#             # mat1[student_val][course_code] = grade_val
+#             semester_val = row[2].split()
+#             semester_val = semester_val[len(semester_val)-1]
+#             mat1[student_val][(course_code,'semester')] = (grade_val,semester_val)
+#             # mat1[student_val]['semester'] = semester_val
+#         line_count+=1
 
-addval = len(mat1)
-with open('data/csv/3.csv') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    line_count = 0
-    for row in csv_reader:
-        if line_count != 0:
-            student_val = row[1]
-            student_val = int(student_val.split('Student ')[1]) + addval
-            if student_val not in mat1:
-                temp = copy.deepcopy(subjects_list)
-                mat1[student_val] = temp
-            course_code = row[len(row)-6]
-            grade_val = grade_scale[row[len(row)-3]]
-            mat1[student_val][course_code] = grade_val
-            semester_val = row[2].split()
-            semester_val = semester_val[len(semester_val)-1]
-            mat1[student_val]['semester'] = semester_val
-        line_count+=1
+# addval = len(mat1)
+# with open('data/csv/3.csv') as csv_file:
+#     csv_reader = csv.reader(csv_file, delimiter=',')
+#     line_count = 0
+#     for row in csv_reader:
+#         if line_count != 0:
+#             student_val = row[1]
+#             student_val = int(student_val.split('Student ')[1]) + addval
+#             if student_val not in mat1:
+#                 temp = copy.deepcopy(subjects_list)
+#                 mat1[student_val] = temp
+#             course_code = row[len(row)-6]
+#             grade_val = grade_scale[row[len(row)-3]]
+#             # mat1[student_val][course_code] = grade_val
+#             semester_val = row[2].split()
+#             semester_val = semester_val[len(semester_val)-1]
+#             mat1[student_val][(course_code,'semester')] = (grade_val,semester_val)
+#             # mat1[student_val]['semester'] = semester_val
+#         line_count+=1
 
-addval = len(mat1)
-with open('data/csv/4.csv') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    line_count = 0
-    for row in csv_reader:
-        if line_count != 0:
-            student_val = row[1]
-            student_val = int(student_val.split('Student ')[1]) + addval
-            if student_val not in mat1:
-                temp = copy.deepcopy(subjects_list)
-                mat1[student_val] = temp
-            course_code = row[len(row)-6]
-            grade_val = grade_scale[row[len(row)-3]]
-            mat1[student_val][course_code] = grade_val
-            semester_val = row[2].split()
-            semester_val = semester_val[len(semester_val)-1]
-            mat1[student_val]['semester'] = semester_val
-        line_count+=1
+# addval = len(mat1)
+# with open('data/csv/4.csv') as csv_file:
+#     csv_reader = csv.reader(csv_file, delimiter=',')
+#     line_count = 0
+#     for row in csv_reader:
+#         if line_count != 0:
+#             student_val = row[1]
+#             student_val = int(student_val.split('Student ')[1]) + addval
+#             if student_val not in mat1:
+#                 temp = copy.deepcopy(subjects_list)
+#                 mat1[student_val] = temp
+#             course_code = row[len(row)-6]
+#             grade_val = grade_scale[row[len(row)-3]]
+#             # mat1[student_val][course_code] = grade_val
+#             semester_val = row[2].split()
+#             semester_val = semester_val[len(semester_val)-1]
+#             mat1[student_val][(course_code,'semester')] = (grade_val,semester_val)
+#             # mat1[student_val]['semester'] = semester_val
+#         line_count+=1
 
-addval = len(mat1)
-with open('data/csv/5.csv') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    line_count = 0
-    for row in csv_reader:
-        if line_count != 0:
-            student_val = row[1]
-            student_val = int(student_val.split('Student ')[1]) + addval
-            if student_val not in mat1:
-                temp = copy.deepcopy(subjects_list)
-                mat1[student_val] = temp
-            course_code = row[len(row)-6]
-            grade_val = grade_scale[row[len(row)-3]]
-            mat1[student_val][course_code] = grade_val
-            semester_val = row[2].split()
-            semester_val = semester_val[len(semester_val)-1]
-            mat1[student_val]['semester'] = semester_val
-        line_count+=1
+# addval = len(mat1)
+# with open('data/csv/5.csv') as csv_file:
+#     csv_reader = csv.reader(csv_file, delimiter=',')
+#     line_count = 0
+#     for row in csv_reader:
+#         if line_count != 0:
+#             student_val = row[1]
+#             student_val = int(student_val.split('Student ')[1]) + addval
+#             if student_val not in mat1:
+#                 temp = copy.deepcopy(subjects_list)
+#                 mat1[student_val] = temp
+#             course_code = row[len(row)-6]
+#             grade_val = grade_scale[row[len(row)-3]]
+#             # mat1[student_val][course_code] = grade_val
+#             semester_val = row[2].split()
+#             semester_val = semester_val[len(semester_val)-1]
+#             mat1[student_val][(course_code,'semester')] = (grade_val,semester_val)
+#             # mat1[student_val]['semester'] = semester_val
+#         line_count+=1
 
-addval = len(mat1)
-with open('data/csv/6.csv') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    line_count = 0
-    for row in csv_reader:
-        if line_count != 0:
-            student_val = row[1]
-            student_val = int(student_val.split('Student ')[1]) + addval
-            if student_val not in mat1:
-                temp = copy.deepcopy(subjects_list)
-                mat1[student_val] = temp
-            course_code = row[len(row)-6]
-            grade_val = grade_scale[row[len(row)-3]]
-            mat1[student_val][course_code] = grade_val
-            semester_val = row[2].split()
-            semester_val = semester_val[len(semester_val)-1]
-            mat1[student_val]['semester'] = semester_val
-        line_count+=1
+# addval = len(mat1)
+# with open('data/csv/6.csv') as csv_file:
+#     csv_reader = csv.reader(csv_file, delimiter=',')
+#     line_count = 0
+#     for row in csv_reader:
+#         if line_count != 0:
+#             student_val = row[1]
+#             student_val = int(student_val.split('Student ')[1]) + addval
+#             if student_val not in mat1:
+#                 temp = copy.deepcopy(subjects_list)
+#                 mat1[student_val] = temp
+#             course_code = row[len(row)-6]
+#             grade_val = grade_scale[row[len(row)-3]]
+#             # mat1[student_val][course_code] = grade_val
+#             semester_val = row[2].split()
+#             semester_val = semester_val[len(semester_val)-1]
+#             mat1[student_val][(course_code,'semester')] = (grade_val,semester_val)
+#             # mat1[student_val]['semester'] = semester_val
+#         line_count+=1
 
-addval = len(mat1)
-with open('data/csv/7.csv') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    line_count = 0
-    for row in csv_reader:
-        if line_count != 0:
-            student_val = row[1]
-            student_val = int(student_val.split('Student ')[1]) +addval
-            if student_val not in mat1:
-                temp = copy.deepcopy(subjects_list)
-                mat1[student_val] = temp
-            course_code = row[len(row)-6]
-            grade_val = grade_scale[row[len(row)-3]]
-            mat1[student_val][course_code] = grade_val
-            semester_val = row[2].split()
-            semester_val = semester_val[len(semester_val)-1]
-            mat1[student_val]['semester'] = semester_val
-        line_count+=1
+# addval = len(mat1)
+# with open('data/csv/7.csv') as csv_file:
+#     csv_reader = csv.reader(csv_file, delimiter=',')
+#     line_count = 0
+#     for row in csv_reader:
+#         if line_count != 0:
+#             student_val = row[1]
+#             student_val = int(student_val.split('Student ')[1]) +addval
+#             if student_val not in mat1:
+#                 temp = copy.deepcopy(subjects_list)
+#                 mat1[student_val] = temp
+#             course_code = row[len(row)-6]
+#             grade_val = grade_scale[row[len(row)-3]]
+#             # mat1[student_val][course_code] = grade_val
+#             semester_val = row[2].split()
+#             semester_val = semester_val[len(semester_val)-1]
+#             mat1[student_val][(course_code,'semester')] = (grade_val,semester_val)
+#             # mat1[student_val]['semester'] = semester_val
+#         line_count+=1
 
-data_matrix = []
+# data_matrix = []
 
-for k,v in mat1.iteritems():
-    temp = []
-    for key,val in v.iteritems():
-        temp.append(val)
-    data_matrix.append(temp)
+# for k,v in mat1.iteritems():
+#     temp = []
+#     for key,val in v.iteritems():
+#         temp.append(val)
+#     data_matrix.append(temp)
 
 
 
