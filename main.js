1d9a12fe21fbf50d0f9d9dcabaa5bb9dcf6d60c3
Sarthika Dhawan <sarthika15170@iiitd.ac.in>
2018-12-02 06:44:34 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+ 
+var express = require('express'); 
+var app = express(); 
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.set('view engine', 'ejs');
+
+app.get('/', function (req, res, next) {
+res.render('main.ejs',{
+});
+});
+
+app.listen(3000, function() { 
+    console.log('server running on port 3000'); 
+} ) 
+
+app.post('/', callName); 
+  
+function callName(req, res) { 
+    
+    var spawn = require("child_process").spawn; 
+      
+    var process = spawn('python',["auto_enc.py", req.body.studid,req.body.sem] ); 
+     
+    
+    process.stdout.on('data', function(data) { 
+    	// console.log(data.toString())
+      var s=JSON.parse(data.toString());
+    	res.render('submit.ejs',{
+ 			'requests':s
+          });
+    } ) 
+} 
